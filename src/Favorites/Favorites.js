fdaf9051d320bfb6e9a210f50b71354f3d44704f
Abdou-MTR <metiriabdou@gmail.com>
2023-04-20 15:28:00 +0000

    update code

---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../favorites.css'
+const Favorites = () => {
+  return (
+    <main>
+        <div className='container'>
+            <div>
+                <h3 className='Favorite-Title'>Favorites</h3>
+                <h6></h6>
+            </div>
+             </div>
+      
+    </main>
+  )
+}
+
+export default Favorites
