1c5032e94f36edf4ae0f16ed446b8adc7cf1aa11
Abdou-MTR <metiriabdou@gmail.com>
2023-04-20 16:46:07 +0000

    update code

--- src/Favorites/FavoriteCard.js
+++ src/Favorites/FavoriteCard.js
@@ -1,17 +1,40 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faXmark, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
 import Favimg from "../images/image 1.png";
 const FavoriteCard = () => {
   return (
     <div>
       <div className="container">
         <div className="row row-cols-1 row-cols-md-2 p-5  ">
-          <div className="col">
+          <div className="col d-flex flex-column FavCard">
             <div className="FavCardImg">
               {" "}
-              <button className="FavRemove d-flex justify-content-end">
-                X
-              </button>
+              <div className=" FavRemove">
+                {" "}
+                <button className="FavRemoveButt">
+                  <FontAwesomeIcon
+                    icon={faXmark}
+                    style={{ color: " var(--var-button-color)" }}
+                  />
+                </button>
+              </div>
               <img src={Favimg} alt="FavImg" />
+            </div>
+            <div className="FavTitle d-flex justify-content-between mt-4">
+              <div>
+                {" "}
+                <h5 className="FavProduct-Title">Samsang Galaxy A53 </h5>
+              </div>
+
+              <div className="d-flex justify-content-end">
+                <button className="FavDownButt">
+                  {" "}
+                  <FontAwesomeIcon icon={faAngleDown} className="FavIcon" />
+                </button>
+              </div>
             </div>
           </div>
         </div>{" "}
