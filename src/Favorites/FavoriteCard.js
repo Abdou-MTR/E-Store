823e14480becf63a813ca54bf33e135f2c546278
Abdou-MTR <metiriabdou@gmail.com>
2023-04-21 03:35:36 +0000

    update code

--- src/Favorites/FavoriteCard.js
+++ src/Favorites/FavoriteCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ShowMore from "react-show-more-button";
 
 import { faXmark, faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
@@ -7,37 +8,47 @@ import Favimg from "../images/image 1.png";
 const FavoriteCard = () => {
   return (
     <div>
-      <div className="container">
-        <div className="row row-cols-1 row-cols-md-2 p-5  ">
-          <div className="col d-flex flex-column FavCard">
-            <div className="FavCardImg">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 ">
+        <div className="col d-flex  flex-column mx-auto FavCard">
+          <div className="FavCardImg">
+            {" "}
+            <div className=" FavRemove">
               {" "}
-              <div className=" FavRemove">
-                {" "}
-                <button className="FavRemoveButt">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: " var(--var-button-color)" }}
-                  />
-                </button>
-              </div>
-              <img src={Favimg} alt="FavImg" />
+              <button className="FavRemoveButt">
+                <FontAwesomeIcon
+                  icon={faXmark}
+                  style={{ color: " var(--var-button-color)" }}
+                />
+              </button>
             </div>
-            <div className="FavTitle d-flex justify-content-between mt-4">
-              <div>
-                {" "}
-                <h5 className="FavProduct-Title">Samsang Galaxy A53 </h5>
-              </div>
+            <img src={Favimg} alt="FavImg" />
+          </div>
+          <div className="FavTitle d-flex justify-content-between mt-4">
+            <div>
+              {" "}
+              <h4 className="FavProduct-Title">Samsang Galaxy A53 </h4>
+            </div>
 
-              <div className="d-flex justify-content-end">
-                <button className="FavDownButt">
-                  {" "}
-                  <FontAwesomeIcon icon={faAngleDown} className="FavIcon" />
-                </button>
-              </div>
+            <div className="d-flex justify-content-end">
+              <button className="FavDownButt ">
+                {" "}
+                <FontAwesomeIcon icon={faAngleDown} className="FavIcon" />
+              </button>
             </div>
           </div>
-        </div>{" "}
+          <div>
+            <p className="FavminDesc mt-2">5G 8GB/256GB 46MP</p>
+            <p className="FavminDesc mt-2">Medium</p>
+            <p className="FavminDesc ">$526</p>
+          </div>
+
+          <div class=" d-flex flex-row justify-content-between FavButtons">
+            <button id="FavMore" className=" me-2">
+              More Information
+            </button>
+            <button className="FavAdd">Add to Card</button>
+          </div>
+        </div>
       </div>{" "}
     </div>
   );
