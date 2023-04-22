1cb508507c286afbfd628563eb7e3896319037b6
Abdou-MTR <metiriabdou@gmail.com>
2023-04-23 00:23:21 +0200

    update code

--- src/Bag/BagCard.js
+++ src/Bag/BagCard.js
@@ -7,8 +7,15 @@ const BagCard = () => {
       <div className="part-1 d-flex flex-row">
         <div></div>
         <img src={image} alt="img" className="BagImg" />
-        <div className="column">
-          <h5 className="Bag-Title">Samsung Galaxy A53</h5>
+        <div className="column ms-3">
+          <h5 className="Bag-Title ">Samsung Galaxy A53</h5>
+          <p className="Bag-Dis mt-4">5G 8gb/128gb 48px</p>
+          <p className="Bag-Dis">Weight : 50g</p>
+          <p className="Bag-Dis">Color : Blue</p>
+          <div className="d-flex flex-row">
+            <h6>Add to Favorite</h6>
+            <h6>Add to Favorite</h6>
+          </div>
         </div>
       </div>
       <div className="part-2"></div>
