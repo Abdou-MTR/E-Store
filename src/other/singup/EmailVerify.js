77bf74dc8b64cc402e0ee5ab32267cfc308b246d
Abdou-MTR <metiriabdou@gmail.com>
2023-12-02 21:51:41 +0100

    new commit

--- src/other/singup/EmailVerify.js
+++ src/other/singup/EmailVerify.js
@@ -1,40 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
 const EmailVerify = () => {
-  const history = useHistory();
-  const [validUrl, setValidUrl] = useState(false);
+  const navigate = useNavigate();
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const params = useParams();
 
   useEffect(() => {
     const verifyEmailUrl = async () => {
       try {
-        const url = `http://localhost:5000/api/users/${params.id}/verify/${params.token}`;
-        const { data } = await axios.get(url);
-        setValidUrl(true);
+        // Simulating asynchronous email verification
+
         setShowSuccessMessage(true);
 
         // Redirect to the login page after a delay (e.g., 3 seconds)
         setTimeout(() => {
-          history.push("/login");
+          navigate("/login");
         }, 3000);
       } catch (error) {
-        setValidUrl(false);
+        console.error("Error verifying email:", error);
       }
     };
     verifyEmailUrl();
-  }, [params, history]);
+  }, [navigate]);
 
   return (
-    <div
-      className={`container ${showSuccessMessage ? "blurred-background" : ""}`}
-    >
+    <div className="blurred-background">
       <div className="row">
         <div className="col-md-6">
-          <Toaster />
           {showSuccessMessage && (
             <div className="success-message">
               <h1>Email Verified!</h1>
